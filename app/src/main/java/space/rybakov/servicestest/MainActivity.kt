4bbf571e85f3ed4d4c335c43888171952ae747a8
dmitry <dimryb@bk.ru>
2022-06-12 17:30:37 +0700

    Init commit

---
+++ app/src/main/java/space/rybakov/servicestest/MainActivity.kt
@@ -0,0 +1,17 @@
+package space.rybakov.servicestest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import space.rybakov.servicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
