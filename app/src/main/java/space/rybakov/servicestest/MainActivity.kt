c58248fb99309cdfd8f8f8ba2f0856563a545e42
dmitry <dimryb@bk.ru>
2022-06-13 20:19:36 +0700

    Запуск JobService

--- app/src/main/java/space/rybakov/servicestest/MainActivity.kt
+++ app/src/main/java/space/rybakov/servicestest/MainActivity.kt
@@ -1,5 +1,8 @@
 package space.rybakov.servicestest
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -29,6 +32,18 @@ class MainActivity : AppCompatActivity() {
                 this,
                 MyIntentService.newIntent(this)
             )
+        }
+        binding.jobScheduler.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
+                .setRequiresCharging(true) // только для зарежающихся
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED) // по вайфаю
+                .setPersisted(true) // для запуска после включения устройства
+                .build()
+
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+            jobScheduler.schedule(jobInfo)
         }
     }
 }
